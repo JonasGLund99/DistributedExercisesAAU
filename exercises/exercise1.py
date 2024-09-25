c6de942e0f2d4758276a0877d11bc4fd7fa0f7a9
Jonas Geertsen Lund <jgl21@student.aau.dk>
2024-09-25 09:05:15 +0200

    Update exercise1.py

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -1,3 +1,5 @@
+import random
+
 from emulators.Device import Device
 from emulators.Medium import Medium
 from emulators.MessageStub import MessageStub
@@ -27,6 +29,95 @@ class Gossip(Device):
         # if len(self._secrets) == self.number_of_devices():
         #    return
         return
+
+    def print_result(self):
+        print(f'\tDevice {self.index()} got secrets: {self._secrets}')
+
+class GossipTotalNaive(Device):
+
+    def __init__(self, index: int, number_of_devices: int, medium: Medium):
+        super().__init__(index, number_of_devices, medium)
+        # for this exercise we use the index as the "secret", but it could have been a new routing-table (for instance)
+        # or sharing of all the public keys in a cryptographic system
+        self._secrets = set([index])
+
+    def run(self):
+        for i in range(self.number_of_devices()):
+            if i != self.index():
+                self.medium().send(GossipMessage(self.index(), i, self._secrets))
+
+        while True:
+            ingoing = self.medium().receive()
+            if ingoing is None:
+                continue
+
+            self._secrets = self._secrets.union(ingoing.secrets)
+
+            # the following is your termination condition, but where should it be placed?
+            if len(self._secrets) == self.number_of_devices():
+                return
+
+    def print_result(self):
+        print(f'\tDevice {self.index()} got secrets: {self._secrets}')
+
+class GossipTotalOptimal(Device):
+
+    def __init__(self, index: int, number_of_devices: int, medium: Medium):
+        super().__init__(index, number_of_devices, medium)
+        # for this exercise we use the index as the "secret", but it could have been a new routing-table (for instance)
+        # or sharing of all the public keys in a cryptographic system
+        self._secrets = set([index])
+
+    def run(self):
+        if self.index() != 0:
+            self.medium().send(GossipMessage(self.index(), 0, self._secrets))
+            
+        while True:
+            ingoing = self.medium().receive()
+            if ingoing is None:
+                continue
+            
+            self._secrets = self._secrets.union(ingoing.secrets)
+            
+            if len(self._secrets) == self.number_of_devices():
+                break
+        
+        if self.index() == 0:
+            for i in range (1, self.number_of_devices()):
+                self.medium().send(GossipMessage(self.index(), i, self._secrets))
+        
+        return
+
+    def print_result(self):
+        print(f'\tDevice {self.index()} got secrets: {self._secrets}')
+
+
+class GossipCircleOptimal(Device):
+
+    def __init__(self, index: int, number_of_devices: int, medium: Medium):
+        super().__init__(index, number_of_devices, medium)
+        # for this exercise we use the index as the "secret", but it could have been a new routing-table (for instance)
+        # or sharing of all the public keys in a cryptographic system
+        self._secrets = set([index])
+
+    def run(self):
+        if self.index() == 0:
+            self.medium().send(GossipMessage(self.index(), 1, self._secrets))
+        
+        while True:
+            ingoing = self.medium().receive()
+            if ingoing is None:
+                continue
+            
+            self._secrets = self._secrets.union(ingoing.secrets)
+            
+            if self.index() == self.number_of_devices() - 2 and len(self._secrets) == self.number_of_devices():
+                return
+            
+            self.medium().send(GossipMessage(self.index(), (self.index() + 1) % self.number_of_devices(), self._secrets))
+            
+            if len(self._secrets) == self.number_of_devices():
+                return
 
     def print_result(self):
         print(f'\tDevice {self.index()} got secrets: {self._secrets}')
